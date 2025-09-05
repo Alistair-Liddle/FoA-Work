f004b95340eb72bdd8a4f42bb791561235b3d689
Alistair-Liddle <alistairliddle@hotmail.com>
2025-09-05 16:23:57 +1000

    Added friday week 6 lecture notes

---
+++ Lectures/lec059.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/* Strings are stored as arrarys, no string type only char type */
+/* The underlying array must be large enough to store the full string with 1 additional byte */
